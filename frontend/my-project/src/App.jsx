a157f3b16deb0bfacbfd2c7a04c8db5d03b31c8e
Lavya <lavya203@gmail.com>
2025-10-30 20:58:03 +0530

    setting up

---
+++ frontend/my-project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Dashboard from "./pages/Dashboard";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+     <Dashboard />
+  
+ 
+    </>
+   );
+}
---
+++ frontend/my-project/src/pages/Dashboard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+function Dashboard() {
+  return (
+    <>
+     <body className="bg-black">
+<section class="flex flex-col items-center justify-center h-screen bg-gradient-to-b from-black via-indigo-950 to-black text-white text-center px-6">
+  <h1 class="text-5xl md:text-6xl font-extrabold mb-4">
+    Welcome to <span class="text-purple-900">Laverse</span>
+  </h1>
+
+  
+</section>
+<main>
+    <div className="w-40 h-40">
+         <span className="text-white">Boom</span>
+    </div>
+</main>
+     </body>
+    </>
+  );
+}
+
+export default Dashboard;
